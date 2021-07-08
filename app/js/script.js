5df0cb7614df6db14f7c415d5b99092a9a5dd996
Nikhil Shrestha <nikhilshrestha0@gmail.com>
2021-07-08 20:43:57 +0545

    finished sunside gency

---
+++ app/js/script.js
@@ -0,0 +1,27 @@
+const btnHamburger = document.querySelector('#btnHamburger')
+const body = document.querySelector('body')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
+
+header.addEventListener('click', function () {
+  body.classList.remove('noscroll')
+
+  if (header.classList.contains('open')) {
+    header.classList.remove('open')
+
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-in')
+      element.classList.add('fade-out')
+    })
+    // overlay.classList.remove('fade-in')
+    // overlay.classList.add('fade-out')
+  } else {
+    body.classList.add('noscroll')
+    header.classList.add('open')
+    fadeElems.forEach(function (element) {
+      element.classList.add('fade-in')
+      element.classList.remove('fade-out')
+    })
+  }
+})
